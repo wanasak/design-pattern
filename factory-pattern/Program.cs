d2c3c3a113fe9a221047523614ce1086872ed2ae
Wanasak <u510610433@gmail.com>
2017-02-13 20:04:24 +0700

    factory-pattern

--- factory-pattern/Program.cs
+++ factory-pattern/Program.cs
@@ -5,10 +5,46 @@ using System.Threading.Tasks;
 
 namespace factory_pattern
 {
+    public interface Shape
+    {
+        void Draw();
+    }
+    public class Rectangle : Shape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Rectangle draw.");
+        }
+    }
+    public class Circle : Shape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Circle draw.");
+        }
+    }
+    public class ShapeFactory 
+    {
+        public Shape GetShape(string shapeType)
+        {
+            if (shapeType == null) 
+                return null;
+            if (shapeType.Equals("circle", StringComparison.OrdinalIgnoreCase))
+                return new Circle();
+            if (shapeType.Equals("rectangle", StringComparison.OrdinalIgnoreCase))
+                return new Rectangle();
+            return null;
+        }
+    }
     public class Program
     {
         public static void Main(string[] args)
         {
+            ShapeFactory shapeFactory = new ShapeFactory();
+            Shape shape1 = shapeFactory.GetShape("circle");
+            shape1.Draw();
+            Shape shape2 = shapeFactory.GetShape("rectangle");
+            shape2.Draw();
         }
     }
 }
