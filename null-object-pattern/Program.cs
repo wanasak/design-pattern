7a8dfae143f1fb6960e31a4a78036b1bfb039d62
Wanasak <u510610433@gmail.com>
2017-02-15 00:09:09 +0700

    skeleton

---
+++ null-object-pattern/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace null_object_pattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
